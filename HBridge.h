d8c8e7bfbe2fdd4d7ab369e4eba39ff860c31bfa
Dan <doates625@gmail.com>
2019-10-18 12:17:10 -0700

    Moved description to README

--- HBridge.h
+++ HBridge.h
@@ -1,29 +1,11 @@
 /**
  * @file HBridge.h
- * @brief Class for H-bridge control via Arduino and Mbed platforms
+ * @brief Class for H-bridge control for Arduino and Mbed
  * @author Dan Oates (WPI Class of 2020)
- * 
- * H-bridges are used primarily to bidirectionally drive DC motors. This class
- * controls H-bridges via three control lines:
- * 
- * - pwm: PWM output to control duty cycle (connect to enable line)
- * - fwd: Forward enable digital output
- * - rev: Reverse enable digital output
- * 
- * Braking is done by writing both the fwd and rev lines low.
- * 
- * Dependencies:
- * - Platform: https://github.com/doates625/Platform.git
- * - DigitalOut: https://github.com/doates625/DigitalOut.git
- * - PwmOut: https://github.com/doates625/PwmOut.git
- * - CppUtil: https://github.com/doates625/CppUtil.git
- * 
- * References:
- * - H-Bridges: https://en.wikipedia.org/wiki/H_bridge
  */
 #pragma once
-#include <PwmOut.h>
 #include <DigitalOut.h>
+#include <PwmOut.h>
 
 /**
  * Class Declaration
---
+++ README.md
@@ -0,0 +1,21 @@
+# HBridge
+Class for H-bridge control for Arduino and Mbed  
+Written by Dan Oates (WPI Class of 2020)
+
+### Description
+H-bridges are used primarily to bidirectionally drive DC motors. This class controls H-bridges via three control lines:
+
+- pwm: PWM output to control duty cycle (connect to enable line)
+- fwd: Forward enable digital output
+- rev: Reverse enable digital output
+
+Braking is done by writing both the fwd and rev lines low.
+
+### Dependencies
+- [CppUtil](https://github.com/doates625/CppUtil.git)
+- [Platform](https://github.com/doates625/Platform.git)
+- [DigitalOut](https://github.com/doates625/DigitalOut.git)
+- [PwmOut](https://github.com/doates625/PwmOut.git)
+
+### References
+- [Text](link)
